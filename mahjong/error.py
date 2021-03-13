9ff019509874293de9667cc3ef9cb0d7e6c08675
thgwon-yaho-qcw <gwontaeho96@gmail.com>
2021-03-14 00:11:35 +0900

    feat: implement agari checker
    주어진 손패가 화료형태인지 아닌지 판단하는 함수 구현
    
    Closes #5

--- mahjong/error.py
+++ mahjong/error.py
@@ -4,3 +4,12 @@ class TileInputError(Exception):
 
     def __str__(self):
         return self.tile_string + '은 올바른 타일 입력이 아닙니다.'
+
+
+class TileCountError(Exception):
+    def __init__(self, tile_string, hand_type):
+        self.tile_string = tile_string
+        self.hand_type = hand_type
+
+    def __str__(self):
+        return '{}의 손패 갯수가 3n+{}개가 아닙니다.'.format(self.tile_string, self.hand_type)
--- mahjong/hand.py
+++ mahjong/hand.py
@@ -1,3 +1,4 @@
+from mahjong.constants import HandType
 from mahjong.convert import string_to_counts, string_to_counts_and_call_counts
 from mahjong.validator import Validator
 
@@ -5,7 +6,7 @@ from mahjong.validator import Validator
 class Hand:
     class TileString(Validator):
         def validate(self, value):
-            string_to_counts(value)
+            string_to_counts_and_call_counts(value)
 
     string = TileString()
 
@@ -16,13 +17,26 @@ class Hand:
         return self.string
 
     @property
-    def counts(self):
+    def counts(self) -> tuple[list[int], list[list[int]]]:
         return string_to_counts_and_call_counts(self.string)
 
     @property
-    def concealed_counts(self) -> list[list[int]]:
+    def concealed_counts(self) -> list[int]:
         return self.counts[0]
 
     @property
     def call_counts(self) -> list[list[int]]:
         return self.counts[1]
+
+    @property
+    def is_called(self):
+        return len(self.call_counts) > 0
+
+    @property
+    def hand_type(self):
+        if len(self.call_counts) * 3 + sum(self.concealed_counts) == 14:
+            return HandType.AFTER_DRAW
+        elif len(self.format_34[1]) * 3 + sum(self.format_34[0]) == 13:
+            return HandType.BEFORE_DRAW
+        else:
+            return HandType.INVALID
