6139f947a660c70996c52d354a04efda26a7c9bd
thgwon-yaho-qcw <gwontaeho96@gmail.com>
2021-03-29 17:15:29 +0900

    feat: implement last tile and one shot
    일발 해저 구현

--- mahjong/hand_info.py
+++ mahjong/hand_info.py
@@ -5,10 +5,12 @@ class HandInfo:
             round_wind=None,
             player_wind=None,
             is_ready_hand=False,
-            is_opened=False
+            is_one_shot=False,
+            is_last_tile=False
     ):
         self.is_tsumo = is_tsumo
         self.round_wind = round_wind
         self.player_wind = player_wind
         self.is_ready_hand = is_ready_hand
-        self.is_opened = is_opened
+        self.is_one_shot = is_one_shot
+        self.is_last_tile = is_last_tile
---
+++ mahjong/yaku_list/one_shot.py
@@ -0,0 +1,12 @@
+from mahjong.hand_info import HandInfo
+from mahjong.yaku_list.yaku import Yaku
+
+
+class OneShot(Yaku):
+    def __init__(self):
+        self.han_open = 0
+        self.han_closed = 1
+        self.is_yakuman = False
+
+    def is_satisfied(self, division, hand_info: HandInfo, rule):
+        return hand_info.is_one_shot
