c1879cab8ff3a26cbc52f598f78dd87317e8ce42
thgwon-yaho-qcw <gwontaeho96@gmail.com>
2021-03-29 16:33:30 +0900

    fix: hotfix : rule
    rule 매개변수 부분의 오류 hotfix

--- mahjong/han.py
+++ mahjong/han.py
@@ -8,25 +8,26 @@ def _get_yaku_list():
     return [ReadyHand(), SelfPick(), AllSimples(), SevenPairs()], [ThirteenOrphans()]
 
 
-def calculate_han(division: Division, hand_info: HandInfo):
+def calculate_han(division: Division, hand_info: HandInfo, rule):
     """
     calculate han
     :param division: Division obj
     :param hand_info: HandInfo obj
+    :param rule: Rule obj
     :return: int, boolean
     """
     han = 0
     yaku_list, yakuman_list = _get_yaku_list()
 
     for yakuman in yakuman_list:
-        if yakuman.is_satisfied(division, hand_info, None):
+        if yakuman.is_satisfied(division, hand_info, rule):
             han += yakuman.get_han(division.is_opened)
 
     if han > 0:
         return han, True
 
     for yaku in yaku_list:
-        if yaku.is_satisfied(division, hand_info, None):
+        if yaku.is_satisfied(division, hand_info, rule):
             han += yaku.get_han(division.is_opened)
 
     return han, False
