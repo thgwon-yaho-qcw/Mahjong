507d1b2285a54dc7dd00cb0b370a37d2c77df698
thgwon-yaho-qcw <gwontaeho96@gmail.com>
2021-03-29 22:51:02 +0900

    feat: implement yakus
    해저, 하저, 더블리치, 창깡 구현

--- mahjong/hand_info.py
+++ mahjong/hand_info.py
@@ -5,12 +5,20 @@ class HandInfo:
             round_wind=None,
             player_wind=None,
             is_ready_hand=False,
+            is_double_ready_hand=False,
             is_one_shot=False,
-            is_last_tile=False
+            is_last_tile=False,
+            is_last_discard=False,
+            is_dead_wall_draw=False,
+            is_robbing_a_quad=False
     ):
         self.is_tsumo = is_tsumo
         self.round_wind = round_wind
         self.player_wind = player_wind
         self.is_ready_hand = is_ready_hand
+        self.is_double_ready_hand = is_double_ready_hand
         self.is_one_shot = is_one_shot
         self.is_last_tile = is_last_tile
+        self.is_last_discard = is_last_discard
+        self.is_dead_wall_draw = is_dead_wall_draw
+        self.is_robbing_a_quad = is_robbing_a_quad
---
+++ mahjong/yaku_list/dead_wall_draw.py
@@ -0,0 +1,12 @@
+from mahjong.hand_info import HandInfo
+from mahjong.yaku_list.yaku import Yaku
+
+
+class DeadWallDraw(Yaku):
+    def __init__(self):
+        self.han_open = 1
+        self.han_closed = 1
+        self.is_yakuman = False
+
+    def is_satisfied(self, division, hand_info: HandInfo, rule):
+        return hand_info.is_dead_wall_draw
